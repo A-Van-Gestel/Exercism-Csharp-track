048319ca7a8ab8acd7cb6c148339392bad490927
Axel Van Gestel <axel.van.gestel@gmail.com>
2024-06-07 14:44:49 +0200

    Submitted Annalyn's Infiltration

--- annalyns-infiltration/AnnalynsInfiltration.cs
+++ annalyns-infiltration/AnnalynsInfiltration.cs
@@ -2,23 +2,41 @@ using System;
 
 static class QuestLogic
 {
+    // True if the knight is sleeping (not awake).
     public static bool CanFastAttack(bool knightIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFastAttack() method");
+        return !knightIsAwake;
     }
 
+    // True if at least one is awake.
     public static bool CanSpy(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSpy() method");
+        if (knightIsAwake || archerIsAwake || prisonerIsAwake)
+        {
+            return true;
+        }
+        return false;
     }
 
+    // True if the archer is sleeping (not awake) and the prisoner is awake.
     public static bool CanSignalPrisoner(bool archerIsAwake, bool prisonerIsAwake)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanSignalPrisoner() method");
+        if (!archerIsAwake && prisonerIsAwake)
+        {
+            return true;
+        }
+        return false;
     }
 
+    // True if she has her pet dog with her and the archer is asleep.
+    // True if she doesn't have her pet dog with her, the prisoner is awake and both the knight & archer are sleeping (not awake).
     public static bool CanFreePrisoner(bool knightIsAwake, bool archerIsAwake, bool prisonerIsAwake, bool petDogIsPresent)
     {
-        throw new NotImplementedException("Please implement the (static) QuestLogic.CanFreePrisoner() method");
+        if ((petDogIsPresent && !archerIsAwake) ||
+            (!petDogIsPresent && prisonerIsAwake && !archerIsAwake && !knightIsAwake))
+        {
+            return true;
+        }
+        return false;
     }
 }
